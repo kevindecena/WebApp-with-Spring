8e6156d51cc54433d46de7c54515de877c0fc3d5
Kevin Decena <kcdecena.16@gmail.com>
2023-04-06 21:52:28 +0800

    added target date field in todo page

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,10 +1,18 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+
+@Entity//mapped to data base
 public class Todo {
+
+    public Todo(){}
 
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -16,7 +24,10 @@ public class Todo {
     }
 
 
+    @Id
+    @GeneratedValue
     private int id;
+    //@Column(name="name")
     private String username;
     @Size(min=10, message="Enter atleast 10 chars")
     private String description;
